9bff2c8b485b4482efe9320ff25a4ad9c3af4b5f
Arxareon <33066061+Arxareon@users.noreply.github.com>
2026-04-21 18:48:58 +0200

    Version 3.3 - Release candidate

--- .annotations/.main.lua
+++ .annotations/.main.lua
@@ -2508,6 +2508,60 @@ local ruRU = {
 ---@class database_warband : profileStorage
 ---@field profiles addonProfile[]
 
+	---@class addonProfile : profile
+	---@field data profileData
+
+		---@class profileData
+		---@field customPreset positionPresetData
+		---@field playerSpeed displayData
+		---@field travelSpeed displayData
+		---@field targetSpeed targetSpeedData
+
+			---@class displayData : positionPresetData
+			---@field visibility displayVisibilityData
+			---@field update speedUpdateData
+			---@field value speedValueData
+			---@field font displayFontData
+			---@field background displayBackgroundData
+
+				---@class displayVisibilityData
+				---@field hidden boolean The display is disabled
+				---@field autoHide boolean The display auto-hides when not moving
+				---@field statusNotice boolean Print a visibility notice in chat on load
+
+				---@class speedUpdateData
+				---@field throttle boolean Slow down speed updates
+				---@field frequency number Speed update threshold in seconds
+
+				---@class speedValueData
+				---@field units [boolean, boolean, boolean] Value types to display in order: percentage | yards/s | coords|s
+				---@field fractionals integer Number of fractional digits to show
+				---@field zeros boolean Show trailing zeros
+
+				---@class displayFontData : fontOptionsData
+				---@field colors speedColorList
+
+					---@class speedColorList : textColorData_base
+					---@field yards color
+					---@field percent color
+					---@field coords color
+
+				---@class displayBackgroundData
+				---@field visible boolean The background is shown
+				---@field colors displayBackgroundColorData
+
+					---@class displayBackgroundColorData
+					---@field bg color Background texture color
+					---@field border color Border texture color
+
+			---@class targetSpeedData
+			---@field enabled boolean
+			---@field value speedValueData
+			---@field font tooltipFontData
+
+				---@class tooltipFontData
+				---@field colors speedColorList
+
 ---@class database_character : characterProfileData
 
 ---@class variables_warband : backupboxSettingsData
@@ -2516,70 +2570,7 @@ local ruRU = {
 ---@field mainDisplay displayType
 
 
---[[ PROFILE DATA ]]
-
----@class profileData
----@field customPreset positionPresetData
----@field playerSpeed displayData
----@field travelSpeed displayData
----@field targetSpeed targetSpeedData
-
----@class addonProfile : profile
----@field data profileData
-
---[ Categories ]
-
----@class speedValueData
----@field units [boolean, boolean, boolean] Value types to display in order: percentage | yards/s | coords|s
----@field fractionals integer Number of fractional digits to show
----@field zeros boolean Show trailing zeros
-
----@class speedColorList : textColorData_base
----@field yards color
----@field percent color
----@field coords color
-
----@class displayFontData : fontOptionsData
----@field colors speedColorList
-
---| Speed Display
-
----@class displayData : positionPresetData
----@field visibility displayVisibilityData
----@field update speedUpdateData
----@field value speedValueData
----@field font displayFontData
----@field background displayBackgroundData
-
----@class speedUpdateData
----@field throttle boolean Slow down speed updates
----@field frequency number Speed update threshold in seconds
-
----@class displayVisibilityData
----@field hidden boolean The display is disabled
----@field autoHide boolean The display auto-hides when not moving
----@field statusNotice boolean Print a visibility notice in chat on load
-
----@class displayBackgroundColorData
----@field bg color Background texture color
----@field border color Border texture color
-
----@class displayBackgroundData
----@field visible boolean The background is shown
----@field colors displayBackgroundColorData
-
---| Target Speed
-
----@class tooltipFontData
----@field colors speedColorList
-
----@class targetSpeedData
----@field enabled boolean
----@field value speedValueData
----@field font tooltipFontData
-
-
---[[ MISC ]]
+--[[ SPEED DISPLAY MANAGEMENT ]]
 
 ---@alias displayType
 ---| "playerSpeed"
